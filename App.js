d5472c8003403d9b551bc3b728a05fe014854383
Charaf Alade YESSOUFOU <aladecharaf23@gmail.com>
2023-03-18 16:55:46 +0000

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+} from 'react-native';
 
-export default function App() {
+function SimpleCard() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='border-4 p-2 rounded-md border-blue-600 mb-3'>
+      <View className='flex flex-row justify-between items-center ml-4'>
+        <Text className='text-gray-700 font-semibold text-lg'>Energy</Text>
+        <Pressable>
+          <View className='p-3 bg-blue-600 rounded-md'>
+            <Text className='text-blue-50'>Connect</Text>
+          </View>
+        </Pressable>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar style='auto' />
+      <View className='px-4'>
+        <SimpleCard />
+        <SimpleCard />
+        <SimpleCard />
+        <SimpleCard />
+      </View>
+    </SafeAreaView>
+  );
+}
